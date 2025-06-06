be4cefb5295ad9704cbc18923ac5239d03ca3d88
Anish Timsina <anishtimsina899@gmail.com>
2025-06-06 19:23:26 +0545

    initial

---
+++ overloading/src/overload.java
@@ -0,0 +1,9 @@
+public class overload {
+    public void display(char a){
+        System.out.println(a);
+    }
+
+    public void display(char c,int num){
+        System.out.println(c + "  " + num);
+    }
+}
